1b7f8ffdd2b9d34bcdef17a1e6ede0ee70240a2c
incredibleMJ <1815737676@qq.com>
2015-11-10 22:28:45 +0800

    EaseFun App

---
+++ EaseFun/EaseFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EaseFun
+//
+//  Created by tarena on 15/11/10.
+//  Copyright © 2015年 IncredibleMJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EaseFun/EaseFun/ViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EaseFun
+//
+//  Created by tarena on 15/11/10.
+//  Copyright © 2015年 IncredibleMJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
